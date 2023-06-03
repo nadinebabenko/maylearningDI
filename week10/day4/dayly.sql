461675387d25b8452e8354c16d0326500c3082e7
Nadine Babenko <119753982+nadinebabenko@users.noreply.github.com>
2023-06-03 17:39:46 +0300

    comments here

---
+++ week10/day4/dayly.sql
@@ -0,0 +1,18 @@
+--CREATE TABLE FirstTab (
+  --   id integer, 
+   --  name VARCHAR(10)
+--) ;
+--INSERT INTO FirstTab VALUES
+--(5,'Pawan'),
+--(6,'Sharlee'),
+--(7,'Krish'),
+--(NULL,'Avtaar');
+ 
+--CREATE TABLE SecondTab (
+    --id integer 
+--);
+--INSERT INTO SecondTab VALUES
+--(5),
+--(NULL);
+--SELECT * FROM SecondTab
+
