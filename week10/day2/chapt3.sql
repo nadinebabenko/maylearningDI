6d3a8c8731522691de824e3f1ff51c5032e8a61d
Nadine Babenko <119753982+nadinebabenko@users.noreply.github.com>
2023-06-03 15:40:32 +0300

    comments here

---
+++ week10/day2/chapt3.sql
@@ -0,0 +1,6 @@
+select * from items where price  > 80 ;
+select * from items where price  <= 300 ;
+select * from customers  where last_name = 'Smith' ;
+select * from customers  where first_name != 'Scott' ;
+
+ 
