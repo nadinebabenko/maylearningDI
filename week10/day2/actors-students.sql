8febb5b86ca0e855014d89b75871f234414505e6
Nadine Babenko <119753982+nadinebabenko@users.noreply.github.com>
2023-06-03 17:28:31 +0300

    comments here

---
+++ week10/day2/actors-students.sql
@@ -0,0 +1,3 @@
+insert into students (first_name, last_name,birth_date) values ('nadya','babenko' , '');
+--ERROR:  INSERT has more target columns than expressions
+--here we could see the outcome  with the blanc filed in the birth_date
